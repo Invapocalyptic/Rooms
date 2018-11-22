6508e77158fab53e418fdfdc8543d00e4c04d6ab
Uraji <keshabeneshmak@gmail.com>
2018-11-22 18:59:10 +0200

    Not working

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 
 using namespace std;
-
-int main(){
-    int arrLimit = 100;
-    int arr[arrLimit];
-    for ( int i = 0; i<100; i++){
-        arr[i] = i+1;
+void reverseArray(char array[]){
+    int limit = array.length;
+    int num = limit;
+    char reversedArray[];
+    for (char letter : array){
+        reversedArray[num] = letter
+        num--
     }
-    for ( int elem : arr){
-        int sum = 0;
-        sum += elem;
-        cout << sum << endl;
-    } 
     
-    // for ( int elem : arr){
-    //     cout << elem << endl;
-    // }
+    for(char letter : reversedArray){
+        cout << letter << endl;
+    }
+}
+int main(){
+    int arrLimit = 8;
+    char arr[arrLimit] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    reverseArray(arr);
     return 0;
 } 
