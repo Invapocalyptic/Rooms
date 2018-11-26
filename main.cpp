f788e08f8a22474da47297b42d3c7e3198822e8c
Uraji <keshabeneshmak@gmail.com>
2018-11-26 17:00:46 +0200

    Something new

--- main.cpp
+++ main.cpp
@@ -2,27 +2,29 @@
 
 using namespace std;
 
+int isItIn(char search, int arrLimit, char array[arrLimit]){
+    
+    for ( char letter : arr){
+        if ( search == letter){
+            return 1;
+        }
+        
+    }
+    return 0;
+}
+
 int main(){
-    int arrLimit = 8;
-    char arr[arrLimit] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
-
-    for(char letter : arr){
-        cout << letter << " ";
-    }
-    cout << endl;
-
-    int start = 0;
-    int end = arrLimit-1;
-    while (start < end){
-        char temp = arr[end];
-        arr[end] = arr[start];
-        arr[start] = temp;
-        start++;
-        end--;
-
-    }
-    for(char letter : arr){
-        cout << letter << " ";
+    int arrLim = 8;
+    char arr[arrLim] = {'a','b','c','d','e','f','g','h'};
+    char searchLetter;
+    cout << "is that an ";
+    cin >> searchLetter;
+    cout << " in the list";
+    isItIn(searchLetter, arrLim, arr)
+    if(isItIn == 0){
+        cout << "no match";
+    }else if(isItIn == 1) { 
+        cout << "yes, there is";
     }
 
     return 0;
